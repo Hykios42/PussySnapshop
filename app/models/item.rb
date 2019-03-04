f595083a2d5eac18711214fec0096b55e3d85ba5
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-04 17:06:58 +0100

    [progress] modifs img - ajout gem faker - seed items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5, maximum: 140} 
   validates :description, presence: true, length: {minimum: 10, maximum: 1000}
   validates :price, presence: true, if: :validate_price
-  validates :img_url, presence: true
+  validates :image_url, presence: true
 
   private
 
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
     if price >= 1 && price <= 1000
       return true
     else
-      errors.add{"Price not good ! min 1 / max 1000"}
+      errors.add{"Price's not right ! min 1 - max 1000"}
     end
   end
 

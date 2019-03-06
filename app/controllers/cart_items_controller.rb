bbebfe5952cb2681823bc45b6ce652e54aa30e9f
hicham <hicham.34@hotmail.fr>
2019-03-06 19:10:14 +0100

    panier fini

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,16 @@
 class CartItemsController < ApplicationController
-  def create
-      add_item_to_cart = CartItem.new(cart_id: Cart.find_by(user_id: current_user).id , item_id: params[:item_id])
-      
+  def new
+    @cart_item = CartItem.new
+  end
 
-      if add_item_to_cart.save
+  def create
+    puts "---------------------------"
+    puts Cart.find_by(user_id: current_user).id
+    puts params[:item_id]
+    puts "---------------------------"
+      @add_item_to_cart = CartItem.new(cart_id: Cart.find_by(user_id: current_user).id ,item_id: params[:item_id])
+      if @add_item_to_cart.save
         puts('ok');
-        
       else
         puts('fuckk');
         
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'cart_items/create'
+ 
   devise_for :users
   root to: 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html"
   
   resources  :items, only:  [:index, :show] do
-    resources :cart_items, only: [:create] 
+    resources :cart_items 
   end
 
 end

cceaae010513028399e9074418821d81785d0688
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-07 01:24:06 +0100

    ajout controller et view users#show + modifs devise et routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html"
   
   resources  :items, only:  [:index, :show]
+  resources  :users, only:  [:show]
 
 end

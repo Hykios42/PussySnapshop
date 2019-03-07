346245cac793daccd5bcc66d7c2cc3fa3da659a2
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-08 00:10:00 +0100

    modifs routes temp

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -8,7 +8,7 @@ class CartItemsController < ApplicationController
   end
 
   def create
-      @add_item_to_cart = CartItem.new(cart_id: Cart.find_by(user_id: current_user) ,item_id: params[:item_id])
+      @add_item_to_cart = CartItem.new(cart_id: Cart.find_by(user_id: current_user.id).id, item_id: Item.find(params[:item_id]))
       if @add_item_to_cart.save
         flash[:success] = "COOL !"
         puts('ok')

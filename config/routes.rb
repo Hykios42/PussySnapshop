356cba80dd891068e0ec5503e2e4e89401c2be18
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-07 12:14:29 +0100

    v2 liaison front-back views items + fix devise forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
  
   devise_for :users
   root to: 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html"
   
-  resources  :items, only:  [:index, :show] do
+  resources  :items, only: [:index, :show] do
     resources :cart_items 
   end
-  resources  :users, only:  [:show]
+  resources  :users, only: [:show]
   
 end

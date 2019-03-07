726b3ed159e00ef51214174e38fb47c17904716c
Jimmy Charpagne <jimmy.charpagne@gmail.com>
2019-03-07 17:52:46 +0100

    maj schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_152526) do
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_carts_on_item_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
@@ -53,10 +51,8 @@ ActiveRecord::Schema.define(version: 2019_03_05_152526) do
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
-    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 

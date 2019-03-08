4180649913c54824d9d6f535128ea10cd850acc2
Emma Calvet <emma.calvet.g@gmail.com>
2019-03-08 03:09:21 +0100

    fix cart view

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,20 @@
 class CartController < ApplicationController
+    before_action :authenticate_user!, only: [:show]
+    before_action :own_profile, only: [:show]
+
     def show
         @cartItem = CartItem.all
         @item = Item.all
     end
+
+  private
+
+  def own_profile
+    if params[:user_id].to_i == current_user.id
+    else
+      redirect_to user_path(current_user.id)
+      flash[:error] = "Tu n'as pas l'autorisation"
+    end
+  end
+
 end
